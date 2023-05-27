343f532e983f45f0514a84ec967fd76b24ecdcf8
Margarita <diasimba2014@gmail.com>
2023-05-27 18:52:28 +0300

    Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,24 +3,24 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.WriteLine("Ввдите положительное число: ");
+Console.WriteLine("Введите положительное число: ");
 int Prompt(string message)
 {
-  Console.Write(message);
-  int result = Convert.ToInt32(Console.ReadLine());
-  return result;
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
 }
 
 int NaturalNumber(int n, int m)
 {
-  if (n == m) return n;
-  else Console.Write($"{NaturalNumber(n, m + 1)}, ");
-  return m;
+    if (n == m)
+        return n;
+    else Console.Write($" {NaturalNumber(n, m + 1)}, ");
+    return m;
 }
-
 int n = Prompt("N: ");
 if (n < 1)
 {
-  return;
+    return;
 }
 Console.WriteLine(NaturalNumber(n, 1));
